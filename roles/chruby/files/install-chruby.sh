e0f9effc8752d9c9b6fb192be7107b3b9f0a5e65
Rui Miguel <ravage@fragmentized.net>
2013-07-28 15:40:55 +0100

    Adds roles for:     - app server     - web server     - queue server     - db server

---
+++ roles/chruby/files/install-chruby.sh
@@ -0,0 +1,5 @@
+wget -O chruby-0.3.6.tar.gz https://github.com/postmodern/chruby/archive/v0.3.6.tar.gz
+tar -xzvf chruby-0.3.6.tar.gz
+cd chruby-0.3.6/
+make install
+rm -rf chruby-0.3.6.tar.gz chruby-0.3.6
---
+++ roles/chruby/files/install-ruby-install.sh
@@ -0,0 +1,5 @@
+wget -O ruby-install-0.2.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.2.1.tar.gz
+tar -xzvf ruby-install-0.2.1.tar.gz
+cd ruby-install-0.2.1/
+sudo make install
+rm -rf ruby-install-0.2.1.tar.gz ruby-install-0.2.1
