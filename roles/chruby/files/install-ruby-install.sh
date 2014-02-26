ecbca82d612154aaaf50070dcd196e5f9132ae7e
Rui Miguel <ravage@fragmentized.net>
2014-02-26 19:13:55 +0000

    * Bump ruby-install version * Add python-passlib task * Force sudo on postgresql

--- roles/chruby/files/install-ruby-install.sh
+++ roles/chruby/files/install-ruby-install.sh
@@ -1,4 +1,4 @@
-RI_VERSION=0.3.4
+RI_VERSION=0.4.0
 
 wget -O ruby-install-$RI_VERSION.tar.gz https://github.com/postmodern/ruby-install/archive/v$RI_VERSION.tar.gz
 tar -xzvf ruby-install-$RI_VERSION.tar.gz
